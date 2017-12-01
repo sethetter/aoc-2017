c57022e50d137925dadfcfd6391108f40167ac95
Seth Etter <sethetter@gmail.com>
2017-12-01 08:48:16 -0600

    Day 1, part 2

--- millisecond-01/millisecond01.hs
+++ millisecond-01/millisecond01.hs
@@ -7,9 +7,15 @@ processDigitStr str =
   let pairs = zip str (tail str ++ [head str])
    in sum . map (digitToInt . fst) . filter (uncurry (==)) $ pairs
 
+processDigitStr2 :: String -> Int
+processDigitStr2 str =
+  let halfStrLength = div (length str) 2
+      pairs = zip str ((drop halfStrLength str) ++ (take halfStrLength str))
+   in sum . map (digitToInt . fst) . filter (uncurry (==)) $ pairs
+
 main :: IO ()
 main = do
   inputStr <- readFile "input.txt"
   let input = init inputStr -- Drops the \n at the end
-      answer = processDigitStr input
+      answer = processDigitStr2 input
    in print answer
